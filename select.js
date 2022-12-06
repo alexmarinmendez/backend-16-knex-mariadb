7927b74bb22cb6f4874681b98dfdc34965aa8789
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-12-06 18:05:52 -0500

    feat: add select all and select filter script

---
+++ select.js
@@ -0,0 +1,16 @@
+const options = require('./options/mysql.config');
+const knex = require('knex');
+
+const database = knex(options);
+
+//GET ALL (select * from )
+// database.from('cars').select('*')
+//     .then(response => console.log(response))
+//     .catch(err => console.log(err))
+//     .finally(() => database.destroy())
+
+//FILTER (select * from where)
+database.from('cars').select('*').where('price', '=', '35000')
+    .then(response => console.log(response))
+    .catch(err => console.log(err))
+    .finally(() => database.destroy())
