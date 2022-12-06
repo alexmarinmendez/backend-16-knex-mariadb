58e4a3c6837185dc593588e469fd702d00e12fde
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-12-06 17:42:14 -0500

    feat: createTable script

---
+++ options/mysql.config.js
@@ -0,0 +1,11 @@
+const options = {
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: '16_knex_mariadb'
+    }
+}
+
+module.exports = options;
